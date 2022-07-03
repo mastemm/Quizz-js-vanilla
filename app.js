0d2da578fb097d6f0b6a0739fce6a4892e0c8c0d
MassimaMao <mastem@msn.com>
2022-07-03 20:46:54 +0200

    add condition to compare submit answers to right answers

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const form = document.querySelector('.form-quizz');
 let tableauResults = [];
 
+const reponses = ['c', 'a', 'b', 'a', 'c'];
+const emojis = ['✔️', '✨', '👀', '😭', '👎'];
+const titreResultat = document.querySelector('.resultats h2');
+const texteResultat = document.querySelector('.note');
+const aideResultat = document.querySelector('.aide');
+const toutesLesQuestions = document.querySelectorAll('.question-block');
+let verifTableau = [];
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   /*preventDefault evite le refresh de la page au submit */
@@ -11,6 +19,19 @@ form.addEventListener('submit', (e) => {
       document.querySelector(`input[name="q${i}"]:checked`).value
     );
   }
-  console.log(tableauResults);
+  // console.log(tableauResults);
+  verifFunc(tableauResults);
   tableauResults = [];
 });
+
+function verifFunc(tabResultats) {
+  for (let a = 0; a < 5; a++) {
+    if (tabResultats[a] === reponses[a]) {
+      verifTableau.push(true);
+    } else {
+      verifTableau.push(false);
+    }
+  }
+  console.log(verifTableau);
+  verifTableau = [];
+}
